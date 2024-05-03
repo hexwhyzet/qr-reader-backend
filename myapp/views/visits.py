968cff54145e2a43873434945f19902b82ed4e9d
kabakov <hexwhyzet@gmail.com>
2024-05-04 01:46:00 +0300

    up

--- myapp/views/visits.py
+++ myapp/views/visits.py
@@ -1,7 +1,7 @@
 from rest_framework import status
 from rest_framework.views import APIView
 
-from myapp.serializers import SuccessJsonResponse
+from myapp.serializers import SuccessJsonResponse, VisitSerializer
 from myapp.services.guards import get_guard
 from myapp.services.points import get_point
 from myapp.services.rounds import get_latest_round
@@ -14,7 +14,8 @@ class VisitPointsView(APIView):
             guard = get_guard(guard_id)
             round = get_latest_round(guard)
             point = get_point(point_id)
-            create_visit(round, point)
-            return SuccessJsonResponse(status=status.HTTP_200_OK)
+            visit = create_visit(round, point)
+            serializer = VisitSerializer(visit)
+            return SuccessJsonResponse(data=serializer.data, status=status.HTTP_200_OK)
         except (Exception,) as e:
             return SuccessJsonResponse(success=False, status=status.HTTP_400_BAD_REQUEST)
