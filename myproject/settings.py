8807fd942837c8533f37a0d4ab9869df3569a9df
kabakov <hexwhyzet@gmail.com>
2024-10-04 17:49:26 +0300

    added host to allowed_hosts

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['10.0.2.2', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['10.0.2.2', 'localhost', '127.0.0.1', os.getenv('HOST')]
 
 # Application definition
 
