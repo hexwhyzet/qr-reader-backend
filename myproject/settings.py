1324a49d24c4eda69a9a0046b6d1e729e136c988
kabakov <hexwhyzet@gmail.com>
2025-02-18 10:27:42 +0300

    up

--- myproject/settings.py
+++ myproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('DJANGO_SECRET')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['10.0.2.2', 'localhost', '127.0.0.1', os.getenv('HOST'), 'localhost:8000']
 
