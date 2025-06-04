653a8f383d1a288b086df88a9e943f4867ab8856
kabakov <hexwhyzet@gmail.com>
2025-06-04 23:53:08 +0300

    fixes

--- myapp/urls.py
+++ myapp/urls.py
@@ -7,15 +7,15 @@ from myapp.views.rounds import StartRoundView, EndRoundView, RoundStatusView
 from myapp.views.visits import VisitPointsView
 
 urlpatterns = [
-    path('whoami', UserInfo.as_view(), name='whoami'),
-    path('register_notification_token', RegisterNotificationToken.as_view(), name='register-notification-token'),
+    path('whoami/', UserInfo.as_view(), name='whoami'),
+    path('register_notification_token/', RegisterNotificationToken.as_view(), name='register-notification-token'),
 
-    path('auth/<int:guard_id>', GuardView.as_view(), name='guard'),
+    path('auth/<int:guard_id>/', GuardView.as_view(), name='guard'),
 
-    path('guard/<int:guard_id>/start_round', StartRoundView.as_view(), name='start-round'),
-    path('guard/<int:guard_id>/end_round', EndRoundView.as_view(), name='end-round'),
-    path('guard/<int:guard_id>/round_status', RoundStatusView.as_view(), name='round-status'),
+    path('guard/<int:guard_id>/start_round/', StartRoundView.as_view(), name='start-round'),
+    path('guard/<int:guard_id>/end_round/', EndRoundView.as_view(), name='end-round'),
+    path('guard/<int:guard_id>/round_status/', RoundStatusView.as_view(), name='round-status'),
 
-    path('guard/<int:guard_id>/visit_point/<int:point_id>', VisitPointsView.as_view(), name='visit-point'),
-    path('guard/<int:guard_id>/visit_point/<int:point_id>/add_message', PointMessageView.as_view(), name='add-message'),
+    path('guard/<int:guard_id>/visit_point/<int:point_id>/', VisitPointsView.as_view(), name='visit-point'),
+    path('guard/<int:guard_id>/visit_point/<int:point_id>/add_message/', PointMessageView.as_view(), name='add-message'),
 ]
