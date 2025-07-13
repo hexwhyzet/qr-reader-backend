d8ffce7aebc768ed2155cff07c1e7f36b99127d5
kabakov <hexwhyzet@gmail.com>
2025-07-13 12:45:52 +0300

    up

--- myapp/utils.py
+++ myapp/utils.py
@@ -1,17 +1,24 @@
-import os
-
-from pyfcm import FCMNotification
+from django.core.management import call_command
 
 from myproject.settings import AUTH_USER_MODEL
 
 
+def telegram_notification(tg_user_id, message):
+    """
+    Отправить уведомление пользователю через вашу management-команду.
+    """
+    call_command('sendnotification', str(tg_user_id), message)
+
+
 def send_fcm_notification(user: AUTH_USER_MODEL, title, body, data=None):
-    fcm = FCMNotification(service_account_file=os.getenv('PATH_TO_GOOGLE_OAUTH_TOKEN'),
-                          project_id=os.getenv('FIREBASE_PROJECT_ID'))
-    if user.device.notification_token is not None:
-        result = fcm.notify(
-            fcm_token=user.device.notification_token,
-            notification_title=title,
-            notification_body=body,
-        )
-        return result
+    # fcm = FCMNotification(service_account_file=os.getenv('PATH_TO_GOOGLE_OAUTH_TOKEN'),
+    #                       project_id=os.getenv('FIREBASE_PROJECT_ID'))
+    # if user.device.notification_token is not None:
+    #     result = fcm.notify(
+    #         fcm_token=user.device.notification_token,
+    #         notification_title=title,
+    #         notification_body=body,
+    #     )
+    #     return result
+    if user.telegram_user_id is not None:
+        telegram_notification(user.telegram_user_id, title + '\n' + body)
